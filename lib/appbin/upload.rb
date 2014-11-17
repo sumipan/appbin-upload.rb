7284e7ea886defef9ff301429e803497b018f52b
takashi nagayasu <regist@g-onion.org>
2014-11-17 19:58:56 +0900

    Add

---
+++ lib/appbin/upload.rb
@@ -0,0 +1,7 @@
+require "appbin/upload/version"
+
+module Appbin
+  module Upload
+    # Your code goes here...
+  end
+end
---
+++ lib/appbin/upload/version.rb
@@ -0,0 +1,5 @@
+module Appbin
+  module Upload
+    VERSION = "0.0.1"
+  end
+end
